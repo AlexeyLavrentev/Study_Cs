e8c06c926847d41a4750078877b9b057b3f45418
Aleksey Lavrentev <eastwaan5@yandex.ru>
2022-08-28 09:19:00 +0500

    Добавил решение задачи 4

--- lesson_01/task_04/Program.cs
+++ lesson_01/task_04/Program.cs
@@ -1,2 +1,16 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
 // а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int current = 2;
+if (num > 1)
+{
+    while (current <= num)
+    {
+        Console.Write($"{current}");
+        current += 2;
+        if (current <= num) Console.Write(", ");
+    }
+}
+else Console.WriteLine($"Нет последовательности для числа {num}");
