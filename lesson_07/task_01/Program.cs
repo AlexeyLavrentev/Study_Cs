1e09b146acc94a9b090eac8d858291f09f1d408e
Aleksey Lavrentev <eastwaan5@yandex.ru>
2022-09-10 23:29:46 +0500

    Добавил решение задачи 1

---
+++ lesson_07/task_01/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("Введите количество строк");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+void CorrectInput()
+{
+    if (col <= 0 || row <= 0)
+    {
+        Console.WriteLine("Некоректный ввод");
+    }
+}
+
+CorrectInput();
+
+double[,] NewMatrix()
+{
+    double[,] matrix = new double [row,col];
+    Random rnd = new Random();
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0; j<col; j++)
+        {
+        matrix[i,j] = rnd.NextDouble()*200-100;
+        matrix[i,j] = Math.Round(matrix[i,j], 1);
+        }
+    }
+    return matrix;
+}
+
+double[,] matrix = NewMatrix();
+
+void MatrixPrint()
+{
+    for (int i=0; i<row; i++)
+    {
+        Console.WriteLine("");
+        Console.Write("[");
+        for (int j=0; j<col; j++)
+        {
+        Console.Write($"\t {matrix[i,j]} \t");
+        }
+        Console.Write("]");
+    }
+}
+MatrixPrint();
+Console.WriteLine("");
