4c732b8cca826cdc17038b1e85a3321491c56006
Lavrentev Aleksey <eastwaan5@yandex.ru>
2022-09-13 16:04:54 +0500

    Добавил решение задачи 1

---
+++ lesson_08/task_01/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] ArrayMatrix(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(min, max + 1);
+        }
+    }
+    return array;
+
+}
+
+void ArrangeArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - j - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }  
+}
+
+
+
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0)
+            {
+                Console.Write("[");
+            }
+            if (j < array.GetLength(1) - 1)
+            {
+                Console.Write(array[i, j] + "  ");
+            }
+            else
+            {
+                Console.Write(array[i, j] + "]");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arrMatr = ArrayMatrix(3, 4, 1, 9);
+PrintMatrix(arrMatr);
+Console.WriteLine();
+ArrangeArr(arrMatr);
+PrintMatrix(arrMatr);
